befb5783a5e87228ec04c8eaa995b4954b8171af
AdnanSlef <39320372+AdnanSlef@users.noreply.github.com>
2024-09-30 14:26:08 -0400

    Update main.js
    toString() stderr so not null

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@ function getErrorMessageAndStatus(error, command) {
   }
   if (error.message.includes('Command failed')) {
     // return { status: 'fail', errorMessage: 'failed with exit code 1' }
-    return { status: 'fail', errorMessage: error.stderr }
+    return { status: 'fail', errorMessage: error.stderr.toString() }
   }
   return  { status: 'error', errorMessage: error.message }
 }
